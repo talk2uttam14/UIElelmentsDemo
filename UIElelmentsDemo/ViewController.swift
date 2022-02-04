4f05681e93e5448cd937bee63dc5af78f6a13cff
Uttam Kumar Dey <uttam.dey@comviva.com>
2022-02-04 10:05:21 +0530

    Initial Commit

---
+++ UIElelmentsDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIElelmentsDemo
+//
+//  Created by comviva on 04/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
